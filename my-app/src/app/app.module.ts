87f5bccfd6b9c3abcab62dd8d7a5d4290ccd1b3e
Riley <sjh7397@gmail.com>
2022-06-01 16:41:32 +0900

    first commit

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NewComponentComponent } from './new-component/new-component.component';
+import {TodosModule} from "./todos/todos.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NewComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+    TodosModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
